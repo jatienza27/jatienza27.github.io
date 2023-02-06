530dd3e5ea13b43c035d19ff5d37d7a80845231b
Jasmine Atienza <j_atienza@mail.harpercollege.edu>
2023-02-05 23:24:10 -0600

    assignment3edits

--- web200/assignment3/activity2.js
+++ web200/assignment3/activity2.js
@@ -1,1 +1,68 @@
+//Not sure why functions are not displaying.
+const months = 12;
+const days = 365;
+const hours = 8760;
+const minutes = 525600;
+const seconds = 31536000;
 
+main();
+
+function main() {
+    let numOfYears = getYears;
+    let numOfMonths = getMonths;
+    let numOfDays = getDays;
+    let numOfHours = getHours;
+    let numOfMinutes = getMinutes;
+    let numOfSeconds = getSeconds
+    displayResults(numOfYears, numOfMonths, numOfDays, numOfHours, numOfMinutes, numOfSeconds)
+}
+
+function getYears() {
+    let numOfYears = window.prompt("Enter Number of Years");
+    numOfYears = Number(numOfYears);
+    return numOfYears;
+
+}
+
+function getMonths(numOfYears, months) {
+    let numOfMonths = (numOfYears * months);
+    return numOfMonths;
+
+}
+
+function getDays(numOfYears, days) {
+    let numOfDays = (numOfYears * days);
+    return numOfDays;
+}
+
+function getHours(numOfYears, hours) {
+    let numOfHours = (numOfYears * hours);
+    return numOfHours;
+}
+
+function getMinutes(numOfYears, minutes) {
+    let numOfMinutes = (numOfYears * minutes);
+    return numOfYears;
+}
+
+function getSeconds(numOfYears, seconds) {
+    let numOfSeconds = (numOfYears * seconds);
+    return numOfSeconds;
+}
+
+function displayResults(numOfMiles, numOfYards, numOfFeet, numberOfInches, numOfKilometers, numOfMeters, numOfCentimeters) {
+    numOfYards = numOfYards.toFixed(1);
+    numOfFeet = numOfFeet.toFixed(1);
+    numOfInches = numOfInches.toFixed(1);
+    numOfKilometers = numOfKilometers.toFixed(1);
+    numOfMeters = numOfMeters.toFixed(1);
+    numOfCentimeters = numOfCentimeters.toFixed(1);
+    document.getElementById("numOfMiles").innerText = numOfMiles;
+    document.getElementById("numOfYards").innerText = numOfYards;
+    document.getElementById("numOfFeet").innerText = numOfFeet;
+    document.getElementById("numOfFeet").innerText = numOfFeet;
+    document.getElementById("numOfInches").innerText = numOfInches;
+    document.getElementById("numOfKilometers").innerText = numOfKilometers;
+    document.getElementById("numOfMeters").innerText = numOfMeters;
+    document.getElementById("numOfCentimeters").innerText = numOfCentimeters;
+}
--- web200/assignment3/activity3.js
+++ web200/assignment3/activity3.js
@@ -1,1 +1,78 @@
+//Not sure why functions are not displaying.
 
+const foot = 12;
+const yard = 3;
+const mile = 1760;
+const feet = 5280;
+const inch = 63360;
+const kilometer = 1.609344;
+const meter = 1000;
+const centimeter = 100;
+
+main();
+
+function main() {
+    let numOfMiles = getMiles;
+    let numOfYards = getYards;
+    let numOfFeet = getFeet;
+    let numOfInches = getInches;
+    let numOfKilometers = getKilometers;
+    let numOfMeters = getMeters;
+    let numOfCentimeters = getCentimeters;
+    displayResults(numOfMiles, numOfYards, numOfFeet, numOfInches, numOfKilometers, numOfMeters, numOfCentimeters)
+}
+
+function getMiles() {
+    let numOfMiles = window.prompt("Enter Number of Miles");
+    numOfMiles = Number(numOfMiles);
+    return numOfMiles;
+
+}
+
+function getYards(numOfMiles, mile) {
+    let numOfYards = (numOfMiles * mile);
+    return numOfYards;
+
+}
+
+function getFeet(numOfMiles, feet) {
+    let numOfFeet = (numOfMiles * feet);
+    return numOfFeet;
+}
+
+function getInches(numOfMiles, inch) {
+    let numOfInches = (numOfMiles * inch);
+    return numOfInches;
+}
+
+function getKilometers(numOfMiles, kilometer) {
+    let numOfKilometers = (numOfMiles * kilometer);
+    return numOfKilometers;
+}
+
+function getMeters (numOfMiles, kilometer, meter) {
+    let numOfMeters = ((numOfMiles * kilometer) * meter);
+    return numOfMeters;
+}
+
+function getCentimeters (numOfMiles, kilometer, meter, centimeter) {
+    let numOfCentimeters = (((numOfMiles * kilometer) * meter) * centimeter);
+    return numOfCentimeters;
+}
+
+function displayResults(numOfMiles, numOfYards, numOfFeet, numberOfInches, numOfKilometers, numOfMeters, numOfCentimeters) {
+    numOfYards = numOfYards.toFixed(1);
+    numOfFeet = numOfFeet.toFixed(1);
+    numOfInches = numOfInches.toFixed(1);
+    numOfKilometers = numOfKilometers.toFixed(1);
+    numOfMeters = numOfMeters.toFixed(1);
+    numOfCentimeters = numOfCentimeters.toFixed(1);
+    document.getElementById("numOfMiles").innerText = numOfMiles;
+    document.getElementById("numOfYards").innerText = numOfYards;
+    document.getElementById("numOfFeet").innerText = numOfFeet;
+    document.getElementById("numOfFeet").innerText = numOfFeet;
+    document.getElementById("numOfInches").innerText = numOfInches;
+    document.getElementById("numOfKilometers").innerText = numOfKilometers;
+    document.getElementById("numOfMeters").innerText = numOfMeters;
+    document.getElementById("numOfCentimeters").innerText = numOfCentimeters;
+}
