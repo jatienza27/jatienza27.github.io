2fb5d6fba95c5a2a230d1bc66fc37c25a2df1642
Jasmine Atienza <j_atienza@mail.harpercollege.edu>
2023-10-30 21:55:38 -0500

    moreupdates

--- web200/assignment8/activity3.js
+++ web200/assignment8/activity3.js
@@ -1,9 +1,20 @@
-const numOfScores = prompt("How many scores would you like to enter?");
-const score = [0];
+const numOfScores = Number(prompt("How many scores would you like to enter?"));
 
-for (let i = 0; i < numOfScores; i++) {
-   score = prompt("Please enter you score:");
-   const avgScore = score / numOfScores;
-   document.getElementById ("arrays").innerHTML = "Your average score is: " + avgScore;
+array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+function avgScore(array) {
+   var score = 0;
+   var totalScore = 0;
+   var i = 0;
+
+   score = Number(prompt("Please enter you score:"));
+
+   array.forEach(function (score) {
+      totalScore += score;
+      i++;
+   });
+
+   return avgScore = totalScore / numOfScores;
 }
 
+document.getElementById("array").innerHTML = "Your average score is: " + avgScore;
