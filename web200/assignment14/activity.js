9c8af74d6029b29a8c2a7cd5ab6970845f328915
Jasmine Atienza <j_atienza@mail.harpercollege.edu>
2023-12-09 02:27:40 -0600

    updates

--- web200/assignment14/activity.js
+++ web200/assignment14/activity.js
@@ -1,15 +1,31 @@
-function validateForm() {
-    let form = document.getElementById('infoInput');
-    let valid = form.checkValidity();
+function submitForm() {
+    if (validateForm()) {
+        const formData = {
+            firstName: document.getElementById('firstName').value,
+            lastName: document.getElementById('lastName').value,
+            dateOfBirth: document.getElementById('dateOfBirth').value,
+            email: document.getElementById('email').value,
+            phoneNum: document.getElementById('phoneNum').value,
+            address: document.getElementById('address').value,
+            city: document.getElementById('city').value,
+            state: document.getElementById('state').value,
+            postalCode: document.getElementById('postalCode').value,
+            region: document.getElementById('region').value
+        };
 
-    if (valid) {
-        alert('Form submitted successfully!');
+        fetch( {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(formData)
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Success:', data);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
     }
-
-    return valid;
 }
-
-document.getElementById('infoInput').addEventListener('input', function () {
-    let valid = this.checkValidity();
-    document.getElementById('submit').disabled = !valid;
-});
