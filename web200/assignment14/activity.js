ac831b98139e5adca333402a118e1be1a341af2a
Jasmine Atienza <j_atienza@mail.harpercollege.edu>
2023-12-11 12:04:13 -0600

    updates

--- web200/assignment14/activity.js
+++ web200/assignment14/activity.js
@@ -13,7 +13,7 @@ function submitForm() {
             region: document.getElementById('region').value
         };
 
-        fetch( {
+        fetch('jsonplaceholder.typicode.com', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -27,5 +27,28 @@ function submitForm() {
             .catch(error => {
                 console.error('Error:', error);
             });
+
+
+        const getUserEndpoint = 'jsonplaceholder.typicode.com';
+
+
+        fetch(getUserEndpoint, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log('User Data:', data);
+
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+
+
     }
 }
+
+//I was confused and I don't think I did this correctly.
